985bd959d4591107ffbcb0579cc479ab0d093ae3
Kavinda <rdks.kavinda@gmail.com>
2024-10-29 22:44:24 +0530

    Complete

--- SQL Basics.sql
+++ SQL Basics.sql
@@ -473,7 +473,7 @@ JOIN employee_salary sal
 
 
 -- let's compare this to dense rank
-SELECT dem.employee_id, dem.first_name, gender, salary,
+SELECT dem.employee_id, CONCAT(dem.last_name,' ', dem.first_name) AS full_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary desc) row_num,
 Rank() OVER(PARTITION BY gender ORDER BY salary desc) rank_1,
 dense_rank() OVER(PARTITION BY gender ORDER BY salary desc) dense_rank_2 -- this is numerically ordered instead of positional like rank
@@ -481,3 +481,60 @@ FROM employee_demographics dem
 JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 ;
+
+-- Advanced queries
+-- common table expression
+
+WITH CTE_Example AS
+(
+SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary), AVG(salary)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT * FROM CTE_Example
+;
+
+WITH CTE_Example AS
+(
+SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary), AVG(salary) avg_sal
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT AVG(avg_sal) FROM CTE_Example
+;
+
+WITH CTE_Example AS 
+(
+SELECT employee_id, gender, birth_date
+FROM employee_demographics dem
+WHERE birth_date > '1985-01-01'
+),
+CTE_Example2 AS 
+(
+SELECT employee_id, salary
+FROM parks_and_recreation.employee_salary
+WHERE salary >= 50000
+)
+SELECT *
+FROM CTE_Example cte1
+LEFT JOIN CTE_Example2 cte2
+	ON cte1. employee_id = cte2. employee_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
